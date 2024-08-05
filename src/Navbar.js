14554aaccf8db52c9e432b9135e10c9b9ac7d35e
Yash Sakhare <96004797+YSCodez@users.noreply.github.com>
2024-08-05 10:40:57 +0530

    Routing Chnages

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <ul className="Navbar-list">
         <li><a href="/">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="/form">Sign In/Sign Up</a></li>
+        <li><a href="/login">Sign In/Sign Up</a></li>
       </ul>
     </nav>
   );
