fe7c9483b54263d5e3bef1835e62c8ba7944b31f
Yash Sakhare <96004797+YSCodez@users.noreply.github.com>
2024-08-06 17:23:20 +0530

    Update Button link

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -8,7 +8,7 @@ function App() {
         <h2>Figma to Code Ch2</h2>
         <h1>Forget About Code</h1>
         <p>Leverage AI to automate tasks, gain insights, and streamline processes. Focus on creativity while AI handles complexities, boosting efficiency and unlocking new potential.</p>
-        <a href='/login"'>Get Started</a>
+        <a href='/login'>Get Started</a>
       </div>
     </section>
   );
